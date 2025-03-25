cdd79c020c444d0025335a76eca386c0115669c8
Nithish Aathreya <121860668+Nithish-Aathreya@users.noreply.github.com>
2025-03-25 18:13:50 +0530

    Create sequencer.sv

---
+++ sequencer.sv
@@ -0,0 +1,13 @@
+class mx_sequencer extends uvm_sequencer#(seqitem);
+  `uvm_component_utils(mx_sequencer)
+  
+  
+  function new(string name="mx_sequencer", uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  
+  
+  
+  
+  
+endclass
